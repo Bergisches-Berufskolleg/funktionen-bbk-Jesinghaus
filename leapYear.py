d3f7c67ae004af523af113bd23ba3324dc5fd854
Ben <24-hbfi1.jesinghaus@bbk.schule>
2025-05-05 12:24:15 +0200

    NR 11

--- leapYear.py
+++ leapYear.py
@@ -1,10 +1,20 @@
 
 def isLeapYear(year):
-    # Hier müssen Sie erweitern ...
-    return False
+    if year % 400 == 0:
+        return True
+    elif year % 100 ==0:  
+        return False 
+    elif year % 4 == 0:
+         return True
+    else:
+        return False
+         
+        
+   
 
 def main():
-    isLeapYear(2025)
+    jahr = int(input("Geben Sie ein Jahr ein: "))
+    print(isLeapYear(jahr))
 
 if __name__ == "__main__":
-    main()
+ main()
